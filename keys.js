6ca7031633d5c21b848ad2000ceaa818a1c0c468
Earl Clarin <earl.clarin@gmail.com>
2018-01-26 20:38:42 -0800

    first commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'm4SgVqlodrHYOj9OvrKBzwQEm',
+  consumer_secret: 'ezViKERmehFLYGfwrJFiJ4uCstndd7NRDciKMeEvtStHktUq0X',
+  access_token_key: '25558310-UlFauAhdX4X3BWxmIXtTSYzZc0xPIxC0fnWnkrXwz',
+  access_token_secret: '6etLOrvqR8VDqnxDsDQRDgL6RkLOvnJmEUDUoPnNyQfUm',
+}
+
+exports.tmdbKey = '8d5f41f6535c312ee8ea248cecd733f7';
